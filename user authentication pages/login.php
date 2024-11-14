bfbb291546e1e38b650eef846b80ba6aa321c722
rytays88 <ryan.tays88@gmail.com>
2024-11-14 13:09:04 -0600

    Edited login and register php files w nav bar. Created location.php that checks login status and have an interactive map.

--- user authentication pages/login.php
+++ user authentication pages/login.php
@@ -52,33 +52,58 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <meta charset="utf-8">
 <title>Login Page</title>
 	<link href="assets/css/style.css" rel="stylesheet">
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link href="assets/css/justified-nav.css" rel="stylesheet">
+    <link href="assets/css/styles.css" rel="stylesheet"> 
+    <style>
+        .container { max-width: 400px; margin-top: 50px; }
+        .header_masthhead { margin-bottom:30px; }
+    </style>
 </head>
 
 <body>
-	<div class="welcome">
-		<h3>Welcome to the login page!</h3>
-	</div>
-	<div class="container">
-		<div class="login-box">
-			<h2>Login</h2>
-			<form action="login.php" method="POST"> <!--# is where the data is going to be sent, need to change this later!-->
-				<input type="text" id="username" name="username" required placeholder="Username"
-                    value="<?= isset($_COOKIE['username']) ? htmlspecialchars($_COOKIE['username']) : '' ?>"> 
-                <input type="password" id="password" name="password" required placeholder="Password">
-				<div class="options">
-					<input type="checkbox" id="rememberMe" name="rememberMe">
-					<label for="rememberMe">Remember me</label>
-					<a href="#">Forgot Password?</a> 									<!--change # to the page you want to go to!-->
-				</div>
-				<button type="submit">Login</button>
+<div class="container-fluid">
+    <div class="header masthead">
+        <h3 class="text-muted">Login</h3>
+        <nav>
+            <ul class="nav nav-justified">
+                <li class="nav-item "><a class="nav-link" href="sampleindex.php">HOME</a></li>
+		        <li class="nav-item"><a class="nav-link" href="about.html">ABOUT US</a></li>
+		        <li class="nav-item"><a class="nav-link" href="location.php">Location</a></li>
+
+                <?php if (isset($_SESSION['user_id'])): ?>
+                    <!-- Show Logout if user is logged in -->
+                    <li class="nav-item"><a class="nav-link" href="logout.php">LOGOUT</a></li>
+                <?php else: ?>
+                    <!-- Show Login if user is not logged in -->
+                    <li class="nav-item"><a class="nav-link" href="login.php">LOGIN</a></li>
+                <?php endif; ?>   
+            </ul>
+        </nav>
+    </div>
+</div>
+
+<div class="container">
+	<div class="login-box">
+		<h2>Login</h2>
+		<form action="login.php" method="POST"> <!--# is where the data is going to be sent, need to change this later!-->
+			<input type="text" id="username" name="username" required placeholder="Username"
+                value="<?= isset($_COOKIE['username']) ? htmlspecialchars($_COOKIE['username']) : '' ?>"> 
+            <input type="password" id="password" name="password" required placeholder="Password">
+			<div class="options">
+				<input type="checkbox" id="rememberMe" name="rememberMe">
+				<label for="rememberMe">Remember me</label>
+				<a href="#">Forgot Password?</a> 									<!--change # to the page you want to go to!-->
+			</div>
+			<button type="submit">Login</button>
                 
-                <?php if (isset($error)) : ?>
-                    <div class="alert alert-danger mt-3"><?= htmlspecialchars($error) ?></div>
-                <?php endif; ?>
+            <?php if (isset($error)) : ?>
+                <div class="alert alert-danger mt-3"><?= htmlspecialchars($error) ?></div>
+            <?php endif; ?>
 				
-                <p>Don't have an account? <a href="register.php" id="register">Register</a></p> 	<!--change # to the page you want to go to!-->
-			</form>
-		</div>
+            <p>Don't have an account? <a href="register.php" id="register">Register</a></p> 	<!--change # to the page you want to go to!-->
+        </form>
 	</div>
+</div>
 </body>
 </html>
--- user authentication pages/register.php
+++ user authentication pages/register.php
@@ -37,12 +37,33 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <meta charset="utf-8">
         <title>Register | Music Venue</title>
         <link href="assets/css/style.css" rel="stylesheet"> <!-- same style.css as login page -->
+        <link href="assets/css/bootstrap.css" rel="stylesheet">
+        <link href="assets/css/justified-nav.css" rel="stylesheet">
+        <link href="assets/css/styles.css" rel="stylesheet"> 
     </head>
 
     <body>
-        <div class="welcome">
-            <h3>Welcome to the registration page!</h3>
+        <div class="container-fluid">
+            <div class="header masthead">
+                <h3 class="text-muted">Register</h3>
+                <nav>
+                    <ul class="nav nav-justified">
+                        <li class="nav-item "><a class="nav-link" href="sampleindex.php">HOME</a></li>
+		                <li class="nav-item"><a class="nav-link" href="about.html">ABOUT US</a></li>
+		                <li class="nav-item"><a class="nav-link" href="location.php">Location</a></li>
+
+                        <?php if (isset($_SESSION['user_id'])): ?>
+                            <!-- Show Logout if user is logged in -->
+                            <li class="nav-item"><a class="nav-link" href="logout.php">LOGOUT</a></li>
+                        <?php else: ?>
+                            <!-- Show Login if user is not logged in -->
+                            <li class="nav-item"><a class="nav-link" href="login.php">LOGIN</a></li>
+                        <?php endif; ?>   
+                    </ul>
+                </nav>
+            </div>
         </div>
+
         <div class="container">
             <div class="login-box">
                 <h2>Register</h2>
